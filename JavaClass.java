4ba75fff859d4176a0d1afe967ed1fb741f38365
Duygu Eylül KARATAŞ <96980093+duygueylul@users.noreply.github.com>
2023-10-31 23:03:11 +0300

    Add files via upload

---
+++ JavaClass.java
@@ -0,0 +1,21 @@
+package JavaTemel;
+
+public class JavaClass {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Calculator machine1 = new Calculator();
+		machine1.n1 = 10;
+		machine1.n2 = 20;
+		System.out.println(machine1.n1);
+		
+		
+		Calculator machine2 = new Calculator();
+		machine2.n1 =20;
+		System.out.println(machine2.n1);
+
+	}
+
+}
